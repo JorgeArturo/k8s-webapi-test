ad450a9e151ae93fa470a2b84d4eee571d940cdb
JorgeArturo <ing.jorge.mecatronico@gmail.com>
2026-04-15 21:53:01 -0600

    webapi code init

---
+++ Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using MicroserviceDemo.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+//to access to the API from outside the VPN
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
+// DB
+builder.Services.AddDbContext<AppDbContext>(options =>options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Controllers
+builder.Services.AddControllers();
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddOpenApi("v1");
+//builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+// Swagger UI
+//app.UseSwagger();
+//app.UseSwaggerUI();
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapControllers();
+app.UseAuthorization();
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
