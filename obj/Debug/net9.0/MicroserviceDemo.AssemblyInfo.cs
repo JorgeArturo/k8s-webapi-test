3bd90836ba0a502a9b533ff4bc89cd392bf81967
JorgeArturo <ing.jorge.mecatronico@gmail.com>
2026-04-18 16:20:23 -0600

    internal ip and port

--- obj/Debug/net9.0/MicroserviceDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/MicroserviceDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicroserviceDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca7f11bb96cbe9a15e2cf298e4b1c1020a015de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e373888d4ff57d902be7107b2ddcc2ca26a34f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicroserviceDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicroserviceDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
